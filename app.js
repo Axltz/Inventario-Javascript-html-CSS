751bc00485f01003dcd47201251f8a0e909e563c
Axel Guillermo Martinez Martinez <axelmtz9880@gmail.com>
2025-01-18 13:13:00 -0600

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Juego del numero secreto";
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Indica un numero del 1 al 10";
-
-function intentoDeUsuario(){
-    alert('click desde el botón');
-}
